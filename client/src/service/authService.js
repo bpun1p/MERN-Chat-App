aadd50f34232f92aeac92cf5fb54a27da6d2b9d9
bpun1p <barry_pun@hotmail.com>
2023-07-20 11:45:30 -0700

    user login/register auth services created

---
+++ client/src/service/authService.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const currentUrl = 'http://localhost:3000'
+
+export const registerUser = async (email, password) => {
+  try {
+    const res = await axios.post(`${currentUrl}/access/register`, {
+      email: email,
+      password: password
+    })
+    return res
+  }
+  catch(err) {
+    return err;
+  }
+}
+
+export const loginUser = async (email, password) => {
+  try {
+    const res = await axios.post(`${currentUrl}/login/login`, {
+      email: email,
+      password: password
+    })
+    return res
+  }
+  catch(err) {
+    return err;
+  }
+}
