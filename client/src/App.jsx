72c95ca79fd321b8baedb55d60c4bcd0f34289b9
bpun1p <barry_pun@hotmail.com>
2023-08-10 14:21:42 -0700

    store template created; provider implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Access from './components/access/AccessPage'
-import AccessNav from './components/accessNav/AccessNav';
+import AccessNav from './components/accessNav/AccessNav'
 import MenuToggler from './components/menu/MenuToggler'
 
 export default function App() {
