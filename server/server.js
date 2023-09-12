81b85cf89cc8e505d7656fedda3aab1ac94029c5
bpun1p <barry_pun@hotmail.com>
2023-09-12 15:17:26 -0700

    include render.com in cors origin

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 //middleware to enable requests between multiple browsers 
 const cors = require('cors')
-app.use(cors({origin: 'http://localhost:5173', credentials: true}))
+app.use(cors({origin: ['http://localhost:5173', 'https://bpun1p-chat-app.onrender.com'], credentials: true}))
 
 //connect to mongodb
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
