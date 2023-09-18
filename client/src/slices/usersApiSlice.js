c5fd2571c4edd993cc93c2783f8c132cb0f5af57
bpun1p <barry_pun@hotmail.com>
2023-09-18 16:08:19 -0700

    including user token in header to be sent to server

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice"
-const base_url = import.meta.env.VITE_URL
-// const base_url = 'https://bpun1p-chat-app-api.onrender.com'
+// const base_url = import.meta.env.VITE_URL
+const base_url = 'https://bpun1p-chat-app-api.onrender.com'
 
 export const usersApiSlice = apiSlice.injectEndpoints({         //creates the endpoint and then inject into the store/builder
   endpoints: (builder) => ({
@@ -28,7 +28,8 @@ export const usersApiSlice = apiSlice.injectEndpoints({         //creates the en
         url: `${base_url}/auth/logout`,
         method: 'POST',
         body: data,
-        credentials: 'include'
+        credentials: 'include',
+        withCredentials: true
       })
     }),
     updateUser: builder.mutation({
