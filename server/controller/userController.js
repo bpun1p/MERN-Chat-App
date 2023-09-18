15f729d49e5d2ddbecfb125c51eff09d286475cd
bpun1p <barry_pun@hotmail.com>
2023-09-18 15:43:16 -0700

    correct path set to logoutUser fixed

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -30,7 +30,7 @@ const loginUser = async (req, res) => {
       throw Error('Incorrect password')
     }
     const token = createToken(user_id, email, name)
-    res.cookie('token', token, {path:'/', sameSite:'none', secure:true}).status(200).json({email, name, user_id, token})
+    res.cookie('token', token, {sameSite:'none', secure:true}).status(200).json({email, name, user_id, token})
     // res.status(200).json({email, name, user_id, token})
 
   } 
@@ -76,7 +76,8 @@ const registerUser = async (req, res) => {
 }
 
 const logoutUser = async (req, res) => {
-  res.clearCookie('token', {}, {sameSite:'none', secure:true}).status(200).json({message: 'Logged out'})
+  res.clearCookie('token', {}, {path:'/',sameSite:'none', secure:true}).status(200).json({message: 'Logged out'})
+  res.end()
   // res.status(200).json({message: 'Logged out'})
 }
 
