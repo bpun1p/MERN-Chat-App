0f23dad0fe7aa4fc4d825313bc38b925a48913d0
bpun1p <barry_pun@hotmail.com>
2023-11-07 14:28:08 -0800

    remove console logs and lint Actitrack component

--- client/src/components/actiTrack/ActiTrack.jsx
+++ client/src/components/actiTrack/ActiTrack.jsx
@@ -4,8 +4,8 @@ import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
   LineElement,
-  CategoryScale, //x-axis
-  LinearScale, //y-axis
+  CategoryScale, // x-axis
+  LinearScale,    // y-axis
   PointElement,
   Legend,
   Tooltip
@@ -18,63 +18,60 @@ ChartJS.register(
   PointElement,
   Legend,
   Tooltip
-)
+);
 
 export default function ActiTrack() {
-  const month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const today = new Date();
   const currMonth = month[today.getMonth()];
   const { user } = useSelector((state) => state.auth);
   const [isVisitorCount, setVisitorCount] = useState([]);
-  const [islast7Dates, setLast7Dates] = useState([]);
+  const [isLast7Dates, setLast7Dates] = useState([]);
 
   useEffect(() => {
     const visitorCount = user.visitorData.map(data => data.visitorCount);
     const last7Dates = user.visitorData.map(data => `${data.month}/${data.day}`);
     setVisitorCount(visitorCount);
     setLast7Dates(last7Dates);
-  }, [])
+  }, []);
 
   const data = {
-    labels: islast7Dates,
+    labels: isLast7Dates,
     datasets: [{
       label: `Visitors of the week of ${currMonth}`,
       data: isVisitorCount,
       borderColor: 'blue',
       pointBorderColor: 'blue',
-      tension: 0.4
-    }]
+      tension: 0.4,
+    }],
   };
 
   const options = {
-    plugs: {
-      legend: true
+    plugins: {
+      legend: true,
     },
     scales: {
       y: {
-        max: 30
-      }
-    }
-  }
-  console.log(user.visitorData)
+        max: 30,
+      },
+    },
+  };
+
   return (
-    <>
-      <div style={
-        {
-          width: '600px', 
-          height: '300px', 
-          padding: '20px', 
-          position: 'absolute',
-          bottom: '25px',
-          left: '25px', 
-          backgroundColor: 'white', 
-          borderRadius: '10px'
-        }}>
-        <Line
-          data={data}
-          options={options}
-        ></Line>
-      </div>
-    </>
+    <div style={{
+      width: '600px',
+      height: '300px',
+      padding: '20px',
+      position: 'absolute',
+      bottom: '25px',
+      left: '25px',
+      backgroundColor: 'white',
+      borderRadius: '10px',
+    }}>
+      <Line
+        data={data}
+        options={options}
+      />
+    </div>
   );
 }
